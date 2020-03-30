ce56e7a96b6199cdd02c630f3de0ec4f5873e328
lamboktulus1379 <lamboktulus1379@gmail.com>
2020-03-30 18:27:13 +0700

    Add project files.

---
+++ DefanginganIPAddress_LeetCode/DefanginganIPAddress_LeetCode.cpp
@@ -0,0 +1,42 @@
+// DefanginganIPAddress_LeetCode.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string toappend = "[.]";
+        string toSearch = ".";
+        size_t pos = address.find(toSearch);
+        
+        while (pos != string::npos) {            
+             address.replace(pos, 1, toappend);
+             pos = address.find(toSearch, pos + toappend.length());
+        }        
+        return address;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    string address;
+    cin >> address;
+
+    cout << solution.defangIPaddr(address);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
